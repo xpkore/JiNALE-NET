851a9543810d39dcc7fa4882019491cc2ab6c644
esterTion <lcz970@126.com>
2020-11-12 21:41:45 +0800

    201112: refactor login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,35 +6,49 @@
     </p>
     <form @submit.prevent="doLogin">
       <p class="input-field">
-        <input type="text" id="userid" name="userid"/>
+        <input type="text" id="userid" name="userid" v-model="userid"/>
         <label for="userid">User ID</label>
       </p>
       <p class="input-field">
-        <input type="password" id="pwd" name="pwd"/>
+        <input type="password" id="pwd" name="pwd" v-model="pwd"/>
         <label for="pwd">Password</label>
       </p>
-      <input type="submit" value="Login" />
+      <input :disabled="fetching === true" type="submit" value="Login" />
     </form>
+    <p v-if=errorStr>
+      <span>{{ errorStr }}</span>
+    </p>
   </div>
 </template>
 
 <script>
 export default {
+  data: () => ({
+    userid: '',
+    pwd: '',
+    fetching: false,
+    errorStr: ''
+  }),
   methods: {
-    doLogin: e => {
-      const form = e.target
-      form.pwd.setAttribute('disabled', '')
-      const hashedPwd = form.pwd.value
-      fetch('/api/login', {
+    doLogin (e) {
+      this.$data.errorStr = ''
+      this.$data.fetching = true
+      const hashedPwd = this.$data.pwd
+      fetch(this.$store.state.endpoint + '/login', {
         method: 'POST',
         headers: new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' }),
-        body: `id=${encodeURIComponent(form.userid.value)}&hashed_pwd=${hashedPwd}`
+        body: `id=${encodeURIComponent(this.$data.userid)}&hashed_pwd=${hashedPwd}`,
+        credentials: 'include'
       }).then(r => r.json()).then(d => {
         console.log(d)
+        if (d.code !== 0) {
+          this.$data.errorStr = `Login failed: [${d.code}] ${d.msg}`
+        }
+        return new Promise((resolve, reject) => setTimeout(resolve, 1000))
       }).catch((e) => {
         console.error(e)
       }).finally(() => {
-        form.pwd.removeAttribute('disabled')
+        this.$data.fetching = false
       })
     }
   }
