05bd2695e7ce24f095f418e31c04e959cf7be321
esterTion <lcz970@126.com>
2020-11-19 21:32:14 +0800

    log out

--- src/views/Logout.vue
+++ src/views/Logout.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <h6>Log out</h6>
+    <h5>Log out</h5>
     <p>You are about to log out</p>
     <p>Confirm? </p>
     <div class="row">
       <div class="col s1"></div>
       <router-link to="/user/home" class="col s4 waves-effect waves-light btn">Back</router-link>
       <div class="col s2"></div>
-      <a class="col s4 waves-effect waves-light btn danger" @click="doLogout">Confirm</a>
+      <a class="col s4 waves-effect waves-light btn red" @click="doLogout">Confirm</a>
       <div class="col s1"></div>
     </div>
   </div>
@@ -24,6 +24,14 @@ export default {
     doLogout () {
       fetch(this.$store.state.endpoint + '/logout', {
         credentials: 'include'
+      }).then(r => r.json()).then(d => {
+        // manual slow down
+        return new Promise((resolve, reject) => setTimeout(resolve, 1000))
+      }).catch((e) => {
+        console.error(e)
+      }).finally(() => {
+        this.$store.commit('updateLoginInfo')
+        this.$router.push('/')
       })
     }
   }
