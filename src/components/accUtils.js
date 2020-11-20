40f2836d2cfe800d06b1edd014f5ddc1d6441be1
esterTion <lcz970@126.com>
2020-11-20 22:20:40 +0800

    hashPassword.js => accUtils.js

--- src/components/accUtils.js
+++ src/components/accUtils.js
@@ -1,7 +1,7 @@
 import crypto from 'crypto'
 const clientHashSalt = '3012efd1056e7b8bf0f19a79814e906ab0afc81c'
 
-export default function hashPassword (pwd) {
+export function hashPassword (pwd) {
   return crypto.createHmac('sha1', clientHashSalt)
     .update(pwd)
     .digest('hex').toLowerCase()
