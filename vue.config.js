ff0145f48b7a2307f61415c74804306d283b0967
esterTion <lcz970@126.com>
2020-11-21 22:53:56 +0800

    fix config

--- vue.config.js
+++ vue.config.js
@@ -5,30 +5,32 @@ module.exports = {
   filenameHashing: false,
   productionSourceMap: false,
   chainWebpack: config => {
-    // Use query string as hash to defeat caches instead of filename hash
-    config.output.filename('static/[name].js?[contenthash:8]')
-    config.output.chunkFilename('static/[name].js?[contenthash:8]')
-    config
-      .plugin('extract-css')
-      .tap(args => {
-        args[0].filename = 'static/[name].css?[contenthash:8]',
-          args[0].chunkFilename = 'static/[name].css?[contenthash:8]'
-        args[0].allChunks = true
-        return args
-      })
+    if (process.env.NODE_ENV == 'production') {
+      // Use query string as hash to defeat caches instead of filename hash
+      config.output.filename('static/[name].js?[contenthash:8]')
+      config.output.chunkFilename('static/[name].js?[contenthash:8]')
+      config
+        .plugin('extract-css')
+        .tap(args => {
+          args[0].filename = 'static/[name].css?[contenthash:8]',
+            args[0].chunkFilename = 'static/[name].css?[contenthash:8]'
+          args[0].allChunks = true
+          return args
+        })
 
-    // because we moved the hash to the end of the filename the default blacklist
-    // no longer matches .map$ and includes map files on the index.html
-    // so let's update the regex to match the new pattern
-    config
-      .plugin('preload')
-      .tap(args => {
-        args[0].fileBlacklist = [
-          /\.map\?........$/,
-          /hot-update\.js$/
-        ]
-        return args
-      })
+      // because we moved the hash to the end of the filename the default blacklist
+      // no longer matches .map$ and includes map files on the index.html
+      // so let's update the regex to match the new pattern
+      config
+        .plugin('preload')
+        .tap(args => {
+          args[0].fileBlacklist = [
+            /\.map\?........$/,
+            /hot-update\.js$/
+          ]
+          return args
+        })
+    }
   }
   /*configureWebpack: {
     output: {
