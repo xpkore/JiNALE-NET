97022bb954be16c44344718bbeff63b8f21e002a
esterTion <lcz970@126.com>
2020-11-23 18:06:54 +0800

    improve comment

--- src/components/accUtils.js
+++ src/components/accUtils.js
@@ -1,12 +1,22 @@
 import crypto from 'crypto'
 const clientHashSalt = '3012efd1056e7b8bf0f19a79814e906ab0afc81c'
 
+/**
+ * 
+ * @param {string} pwd raw password to hash
+ * @return {string} hmac hashed password
+ */
 export function hashPassword (pwd) {
   return crypto.createHmac('sha1', clientHashSalt)
     .update(pwd)
     .digest('hex').toLowerCase()
 }
 
+/**
+ * 
+ * @param {Response} r fetch response
+ * @return {Promise<string>}
+ */
 export function checkTokenValidity (r) {
   const tokenStatus = r.headers.get('X-Token-Status')
   if (tokenStatus === 'Revoked') {
--- src/components/dateUtils.js
+++ src/components/dateUtils.js
@@ -4,11 +4,52 @@ function padTime (s) {
   return s
 }
 
+/**
+ * 
+ * @param {string} date
+ * @return {Date} date object
+ */
+export function standardizeDate (date) {
+  return new Date(date.replace(/^(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})$/, '$1/$2/$3 $4:$5:$6 +0900'))
+}
+
+/**
+ * 
+ * @param {string} date 
+ * @param {boolean} showSeconds display seconds in result
+ * @return string formatted date
+ */
 export function dateToLocalStr (date, showSeconds = false) {
-  const dateObj = new Date(date)
+  const dateObj = standardizeDate(date)
   let str = [dateObj.getFullYear(), padTime(dateObj.getMonth() + 1), padTime(dateObj.getDate())].join('/') + ' ' + [padTime(dateObj.getHours()), padTime(dateObj.getMinutes())].join(':')
   if (showSeconds) {
     str += ':' + padTime(dateObj.getSeconds())
   }
   return str
 }
+
+const timeagoInstance = {
+  format: function (date) {
+    date = new Date(date);
+    var ts = date.getTime(), now = Date.now();
+    if (date.getFullYear() == (new Date).getFullYear()) {
+      if (date.getMonth() == (new Date).getMonth()) {
+        if (date.getDate() == (new Date).getDate()) {
+          return padTime(date.getHours()) + ':' + padTime(date.getMinutes());
+        }
+        var day = Math.floor((now - ts) / 24 / 3600e3);
+        return day + ' day' + (day == 1 ? '' : 's') + ' ago';
+      }
+      return padTime(date.getMonth() + 1) + '/' + padTime(date.getDate()) + ' ' + padTime(date.getHours()) + ':' + padTime(date.getMinutes());
+    }
+    return date.getFullYear() + '/' + padTime(date.getMonth() + 1) + '/' + padTime(date.getDate()) + ' ' + padTime(date.getHours()) + ':' + padTime(date.getMinutes());
+  }
+};
+function timeagoRun () {
+  const needs = [].slice.call(document.getElementsByClassName('timeago'));
+  needs.forEach(i => {
+    if (i.hasAttribute('datetime')) i.textContent = timeagoInstance.format(i.getAttribute('datetime'));
+  });
+}
+setInterval(timeagoRun, 6e4);
+window.addEventListener('trigger-timeago', timeagoRun)
