335cd06a7ad35be7b248798395f5688e877e8cea
esterTion <lcz970@126.com>
2020-11-26 21:46:38 +0800

    move music info to server download

--- src/components/accUtils.js
+++ src/components/accUtils.js
@@ -1,5 +1,6 @@
 import crypto from 'crypto'
 const clientHashSalt = '3012efd1056e7b8bf0f19a79814e906ab0afc81c'
+import { initMusicList } from './musicUtils'
 
 /**
  * 
@@ -42,6 +43,7 @@ export async function initMyInfo () {
   }).then(checkTokenValidity.bind(this))
   if (d.code === 0) {
     this.$store.commit('updateLoginInfo', d.data)
+    await initMusicList()
   } else {
     this.$store.commit('updateLoginInfo')
   }
