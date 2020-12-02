636b2b5690b7306978107f781dea527bff0049fc
esterTion <lcz970@126.com>
2020-12-02 13:34:47 +0800

    fix date timezone

--- src/components/dateUtils.js
+++ src/components/dateUtils.js
@@ -10,7 +10,7 @@ function padTime (s) {
  * @return {Date} date object
  */
 export function standardizeDate (date) {
-  return new Date(date.replace(/^(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})$/, '$1/$2/$3 $4:$5:$6 +0900'))
+  return new Date(date.replace(/^(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})$/, '$1/$2/$3 $4:$5:$6 +0800'))
 }
 
 /**
